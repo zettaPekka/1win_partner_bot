53db5ceb2cfc8cac0478ed28c3d3a542e67cc864
Your Name <kigeandaviddd@gmail.com>
2025-09-07 10:30:14 +0300

    commit

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,10 +1,15 @@
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery
-from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from aiogram.filters import Command
+from dotenv import load_dotenv
 
 from middleware.db_di import DatabaseDI
 from database.services.user_service import UserService
 
+import os
+
+
+load_dotenv()
 
 router = Router()
 router.message.middleware(DatabaseDI())
@@ -12,7 +17,9 @@ router.message.middleware(DatabaseDI())
 
 @router.message(Command('next'))
 async def set_next_k(message: Message, user_service: UserService):
-    k = float(message.text.split()[-1])
-    
-    await user_service.set_next_k(message.from_user.id, k)
-    await message.answer(f'Успешно, следующий коэф. выбран: {k}')
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        k = float(message.text.split()[-1])
+        user_id = int(message.text.split()[1])
+        
+        await user_service.set_next_k(user_id, k)
+        await message.answer(f'Успешно, следующий коэф. выбран: {k}')
